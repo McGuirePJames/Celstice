99ef6a05f7df289f1b6dce28ddebf7248c61a8f5
James McGuire <mcguirepjames@gmail.com>
2018-06-12 08:23:36 -0600

    More name changes

---
+++ Selstice/Components/Server/NavigationBar.jsx
@@ -0,0 +1,22 @@
+﻿import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+export class NavigationBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+    handleLogoClick() {
+
+    }
+    render() {
+        return (
+            <nav>
+                
+            </nav>
+        )
+    }
+}
+export default NavigationBar;
